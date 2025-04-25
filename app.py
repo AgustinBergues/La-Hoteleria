00e64b65cf749065978da35f4a197d2b766abb5a
AgustinBergues <abergues@uade.edu.ar>
2025-04-25 02:37:52 -0300

    Version 1.5.2
    Actualizacion de  perspectiva Cliente
    
    Falta:
      - Perspectiva de Empleado
      - Perspectiva de Admin

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
 import json
+import os
 
 app = Flask(__name__)
-app.secret_key = 'tu_clave_secreta'  # Necesaria para usar flash
+app.secret_key = '8273645912037485'  # Necesaria para usar flash
+DATA_FILE = 'static/hotels.json'
 
-
-# Formato de la BD
-#       [
-#           {
-#               "id": 0,
-#               "usuario": "Aguss",
-#               "contraseña": "1234",
-#               "correo": "aguss@example.com",
-#               "permisos": "admin"
-#           }
-#       ]
-
-
-
+impuestos = {
+    "Alquiler_online": 10,  
+    "Mantenimiento": 5       
+}
 
 
 # Cargar usuarios desde el archivo JSON
@@ -26,23 +18,124 @@ def load_users():
         data = json.load(f)
     return data
 
+def cargar_hoteles():
+    with open(DATA_FILE, 'r', encoding='utf-8') as f:
+        return json.load(f)
+    
+def guardar_hoteles(hoteles):
+    with open(DATA_FILE, 'w', encoding='utf-8') as f:
+        json.dump(hoteles, f, indent=4, ensure_ascii=False)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/hoteleria')
-def client():
-    return render_template("client.html")
 
-@app.route('/hoteleria')
-def employee():
-    return render_template("employee.html")
+
+
+
+
+
+@app.route('/client')
+def client():
+    hoteles = cargar_hoteles()
+
+    # Ordenar por visitas, de mayor a menor
+    hoteles.sort(key=lambda h: h['visitas'], reverse=True)
+    hoteles_mas_buscados = hoteles[:3]  # Los 3 más buscados
+
+    # Ordenar por precio más bajo (habitación 'Simple')
+    hoteles.sort(key=lambda h: h['precios']['Simple'])
+    hoteles_mas_economicos = hoteles[:3]  # Los 3 más económicos
+
+   # Mostrar todos los hoteles
+    hoteles_todos = cargar_hoteles()
+
+    return render_template("client.html", hoteles_mas_buscados=hoteles_mas_buscados, hoteles_mas_economicos=hoteles_mas_economicos, hoteles_todos=hoteles_todos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/hotel/<int:hotel_id>')
+def ver_hotel(hotel_id):
+    global hotel_actual
+    hoteles = cargar_hoteles()
+    hotel_actual = hotel_id
+    # Buscar hotel por id
+    hotel = next((h for h in hoteles if h["id"] == hotel_id), None)
+
+    if hotel:
+        hotel["visitas"] += 1  # Sumar visita
+        guardar_hoteles(hoteles)
+
+        return render_template("ver_hotel.html", hotel=hotel)
+    else:
+        return "Hotel no encontrado", 404
+
+@app.route('/formulario')
+def formulario_reserva():
+    hotel_id=hotel_actual
+    return render_template('Form.html', hotel_id=hotel_id)
+
+
+@app.route('/reservar', methods=['POST'])
+def reservar():
+    nombre = request.form.get('name')
+    telefono = request.form.get('phone')
+    correo = request.form.get('email')
+    habitacion = request.form.get('room')
+
+
+
+    print(f"Reserva recibida: {nombre}, {telefono}, {correo}, {habitacion}")
+
+
+    return redirect(url_for('client'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/admin')
 def admin():
     return render_template("admin.html")
 
-@app.route('/ingreso', methods=['GET','POST'])
+@app.route('/form-login', methods=['GET','POST'])
 def login():
     data = load_users()
     _user = request.form['txtuser']
@@ -53,13 +146,15 @@ def login():
             permision = credenciales["permisos"]
             if permision == "cliente":
                 return redirect(url_for('client'))
-            elif permision == "empleado":
-                return redirect(url_for('employee'))
             elif permision == "admin":
                 return redirect(url_for('admin'))
 
 
     return redirect(url_for('home'))
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
