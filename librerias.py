9df07060e963733457e1e115d41d01671752cfab
elperchita <irodriguez3@uade.edu.ar>
2025-06-19 14:02:18 -0300

    v2.3

--- librerias.py
+++
@@ -1,5 +0,0 @@
-import os 
-librerias=["flask","datatime","os","json","werkzeug.security"]
-os.system("pip install -U pip")  # Actualizar pip a la última versión
-for libreria in librerias:
-    os.system(f"pip install {libreria}")
